28a75b92d7073886220af4a8141bce45f221d553
Tomasz <tgmasz@gmail.com>
2022-08-10 00:29:15 +0200

    fixed gitignore, main script draft

---
+++ run_all_tests.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    # 0. install webdrivers automatically (chromedriver, geckodriver, ie driver, safaridriver)
+    # 1. automatically create venv if it does not exist already
+    # 2. check if all dependencies are installed
+    # 3. read input arguments and decide which browser to test
+    # 4. run all tests using pytest
+    ...
