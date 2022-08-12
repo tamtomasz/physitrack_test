ef9319fa401a21677bb040b06c8cec0dcbca3555
Tomasz <tgmasz@gmail.com>
2022-08-12 11:05:08 +0200

    linux setup script tested and fixed

--- setup_and_run_linux.sh
+++ setup_and_run_linux.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
+while getopts b: flag
+do
+    case "${flag}" in
+        b) browser=${OPTARG};;
+    esac
+done
+
 python -m venv ./venv
 ./venv/Scripts/activate
 pip install -r ./requirements.txt
-python -m test_runner -b "$0"
+python -m test_runner -b "$browser"
